7a9714d1e30a9fd9e8a507039e087cd883167cb3
souravdev-eng <105838250+souravmusquaretech@users.noreply.github.com>
2022-06-30 10:38:40 +0530

    Complete signin with google

--- app/src/screens/Auth/LoginScreen/index.tsx
+++ app/src/screens/Auth/LoginScreen/index.tsx
@@ -5,13 +5,35 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './styles';
 import {Icons} from '../../../theme';
 import {useAppNavigation} from '../../../hooks/useAppNavigation';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const LoginScreen = () => {
   const {navigation} = useAppNavigation();
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '689817067732-tva4vg3hejpcd09v296b35nr8ufb6ija.apps.googleusercontent.com',
+    });
+  }, []);
+
+  const signIn = async () => {
+    try {
+      const {user} = await GoogleSignin.signIn();
+      console.log({user});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image source={Icons.Illus1} resizeMode="contain" style={styles.image} />
@@ -20,10 +42,12 @@ const LoginScreen = () => {
         <Image source={Icons.Facebook} style={styles.icon} />
         <Text style={styles.buttonText}>Continue with Facebook</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
+
+      <TouchableOpacity style={styles.button} onPress={signIn}>
         <Image source={Icons.Google} style={styles.icon} />
         <Text style={styles.buttonText}>Continue with Google</Text>
       </TouchableOpacity>
+
       <TouchableOpacity style={styles.button}>
         <Image source={Icons.Apple} style={styles.icon} />
         <Text style={styles.buttonText}>Continue with Apple</Text>
